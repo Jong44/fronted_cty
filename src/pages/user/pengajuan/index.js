f429ded2ddabee37f18011016bdaac1127f5636f
Kenpradana <shaziahuang@gmail.com>
2024-12-06 16:15:51 +0700

    update pengajuan

--- src/pages/user/pengajuan/index.js
+++ src/pages/user/pengajuan/index.js
@@ -4,42 +4,42 @@ import RootLayout from '@/components/global/layout/RootLayout'
 const Pengajuan = () => {
   return (
     <RootLayout>
-      <h1 className='	text-center mb-5'><b>PERMOHONAN KEAMANAN SERTIFIKAT</b></h1>
+      <h1 className='	text-center mb-5 text-3xl'><b>PERMOHONAN KEAMANAN SERTIFIKAT</b></h1>
       <form>
-        <div className='flex'>
-        <div className='flex flex-col '>
-          <label for="name">Nama Lengkap:</label>
-          <input className="border-2 border-black rounded-lg" type="text" id="fname" name="name"/>
+        <div className='flex w-full gap-10'>
+        <div className='flex flex-col gap-3 w-full p-2'>
+          <label for="name">Nama Lengkap</label>
+          <input className="border border-black text-sm rounded-lg outline-none p-2" type="text" id="fname" name="name"/>
         </div>
-        <div className='flex flex-col '>
-          <label for="email">Email:</label>
-          <input className="border-2 border-black rounded-lg" type="text" id="email" name="email"/>
-        </div>
-        </div>
-        <div className='flex'>
-        <div className='flex flex-col '>
-          <label for="notelp">Nomor Telepon:</label>
-          <input className="border-2 border-black rounded-lg" type="text" id="notelp" name="notelp"/>
-        </div>
-        <div className='flex flex-col '>
-          <label for="nik">NIK:</label>
-          <input className="border-2 border-black rounded-lg" type="text" id="nik" name="nik"/>
+        <div className='flex flex-col gap-3 w-full p-2'>
+          <label for="email">Email</label>
+          <input className="border border-black rounded-lg outline-none p-2" type="text" id="email" name="email"/>
         </div>
         </div>
-        <div className='flex flex-col '>
-          <label for="alm">Alamat:</label>
-          <input className="border-2 border-black rounded-lg" type="text" id="alm" name="alm"/>
+        <div className='flex w-full gap-10'>
+        <div className='flex flex-col gap-3 w-full p-2'>
+          <label for="notelp">Nomor Telepon</label>
+          <input className="border border-black rounded-lg outline-none p-2" type="text" id="notelp" name="notelp"/>
         </div>
-        <div className='flex flex-col '>
-          <label for="notelp">File Sertifikat:</label>
+        <div className='flex flex-col gap-3 w-full p-2'>
+          <label for="nik">NIK</label>
+          <input className="border border-black rounded-lg outline-none p-2" type="text" id="nik" name="nik"/>
+        </div>
+        </div>
+        <div className='flex flex-col gap-3 w-full p-2'>
+          <label for="alm">Alamat</label>
+          <input className="border border-black rounded-lg outline-none p-2" type="text" id="alm" name="alm"/>
+        </div>
+        <div className='flex flex-col gap-3 p-2'>
+          <label for="notelp">File Sertifikat</label>
           <input type="file" id="sertif" name="sertif"/>
         </div>
-        <div className='flex flex-col '>
-          <label for="notelp">File KTP:</label>
+        <div className='flex flex-col gap-3 p-2'>
+          <label for="notelp">File KTP</label>
           <input type="file" id="ktp" name="ktp"/>
         </div>
         <div className='grid'>
-        <button type="submit" className='p-4 bg-blue-500 justify-self-end rounded-3xl text-white px-6 py-2' ><b>SUBMIT</b></button>
+        <button type="submit" className='p-4 bg-blue-500 flex justify-self-end items-end rounded-3xl text-white px-6 py-2' ><b>SUBMIT</b></button>
         </div>
       </form>
     </RootLayout>
