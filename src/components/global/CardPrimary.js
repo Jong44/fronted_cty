2e391934ee3e8d86a4574242decc7a7a76f62aa3
unknown <Irgi Alfarizal>
2024-12-06 15:46:36 +0700

    update dashboard

--- src/components/global/CardPrimary.js
+++ src/components/global/CardPrimary.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Index = () => {
+const Index = ({children}) => {
   return (
-    <div className='shadow-md rounded-md bg-white w-[15rem] h-[10rem] p-4'>
-        index
+    <div className='shadow-md rounded-md bg-white w-[15rem] h-[10rem] p-4 flex flex-col justify-center items-center'>
+       {children}
     </div>
   )
 }
