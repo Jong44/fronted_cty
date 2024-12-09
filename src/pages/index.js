814118598ea0f6a5cc66160ce7541d06ded6a925
Jong44 <mat.skak45@gmail.com>
2024-12-10 03:26:06 +0700

    link to login

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/auth/login");
+  }, []);
   return (
    <></>
   );
