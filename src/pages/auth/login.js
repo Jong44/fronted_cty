3eeb0fc594e9b3eb3d8a405b37d1e0c95e7ffdb4
Jong44 <mat.skak45@gmail.com>
2024-12-11 14:27:49 +0700

    yup and formik integration

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,18 +1,31 @@
+import { ErrorMessage, Field, Form, Formik } from 'formik'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useState } from 'react'
+import * as Yup from 'yup'
 
 const Register = () => {
   const router = useRouter()
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const email = e.target.email.value
-    const password = e.target.password.value
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+  const [errors, setErrors] = useState()
 
-    if (!email || !password) {
-      alert('Email dan password harus diisi')
-      return
-    }
+  const schema = Yup.object().shape({
+    email: Yup.string().email().required(),
+    password: Yup.string().required(),
+  })
 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+
+
+  const handleSubmit = async () => {
     router.push('/user/dashboard')
   }
   return (
@@ -26,35 +39,45 @@ const Register = () => {
       <div className="flex-initial w-3/5 bg-gray-100 p-4">
         <div className="h-full flex flex-col justify-center items-center">
           <p className="text-3xl text-center mb-4"><b>Selamat Datang Bos!</b></p>
-          <form className='block justify-center' onSubmit={handleSubmit}>
-            <div>
+          <Formik className='block justify-center'
+            initialValues={formData}
+            validationSchema={schema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting }) => (
+            <Form>
+              <div>
               <label className="block">Email</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Masukkan Email"
+              <div className='flex flex-col'>
+              <Field
+                id="email" name="email" type="text"
                 className="w-[40rem] p-2 mt-2 border border-gray-300 rounded"
               />
+              <ErrorMessage name="email" component={'p'} className='text-red-500'  />
+              </div>
             </div>
             <div className="mt-4">
               <label className="block">Password</label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Masukkan Password"
+              <div className='flex flex-col'>
+              <Field
+                id="password" name="password" type="text"
                 className="w-[40rem] p-2 mt-2 border border-gray-300 rounded"
               />
+              <ErrorMessage name='password' component={'p'} className='text-red-500' />
+              </div>
             </div>
             <p className='flex justify-end text-blue-700'>Belum punya akun? <a href="/auth/register">Daftar di sini</a></p>
             <button
               type="submit"
+              disabled={isSubmitting}
               className="mt-4 w-[40rem] p-2 bg-blue-500 text-white rounded"
             >
               Login
             </button>
-          </form>
+
+            </Form>  
+            )}
+          </Formik>
         </div>
       </div>
     </div>
