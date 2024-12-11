01e534f7f2ae4bdd002fe7b01ef67920aeb09bca
unknown <Irgi Alfarizal>
2024-12-11 20:34:16 +0700

    update pengaturan desain baru

--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import { ErrorMessage, Field, Form, Formik } from 'formik'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
+import * as Yup from 'yup'
 
 const Register = () => {
+  const router = useRouter()
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    passwordConfirm: ''
+  })
+
+  const schema = Yup.object().shape({
+    email: Yup.string().email().required(),
+    password: Yup.string().required(),
+    passwordConfirm: Yup.string().oneOf([Yup.ref('password')], 'Passwords must match').required()
+  })
+
+  const handleSubmit = async () => {
+    router.push('/auth/login')
+  }
+
   return (
     <div className="flex h-screen">
       <div className="flex-initial w-2/5 bg-blue-500">
@@ -12,45 +32,57 @@ const Register = () => {
       <div className="flex-initial w-3/5 bg-gray-100 p-4">
         <div className="h-full flex flex-col justify-center items-center">
           <p className="text-3xl text-center mb-4"><b>Selamat Datang Bos!</b></p>
-          <form className='block justify-center'>
+          <Formik className='block justify-center'
+            initialValues={formData}
+            validationSchema={schema}
+            onSubmit={handleSubmit}
+          >
+            {({ isSubmitting }) => (
+            <Form>
             <div>
               <label className="block">Email</label>
-              <input
+              <Field
                 type="email"
                 id="email"
                 name="email"
                 placeholder="Masukkan Email"
                 className="w-[40rem] p-2 mt-2 border border-gray-300 rounded"
               />
+              <ErrorMessage name="email" component={'p'} className='text-red-500'/>
             </div>
             <div className="mt-4">
               <label className="block">Password</label>
-              <input
+              <Field
                 type="password"
                 id="password"
                 name="password"
                 placeholder="Masukkan Password"
                 className="w-[40rem] p-2 mt-2 border border-gray-300 rounded"
               />
+              <ErrorMessage name="password" component={'p'} className='text-red-500'/>
             </div>
             <div className="mt-4">
               <label className="block">Konfirmasi Password</label>
-              <input
+              <Field
                 type="password"
-                id="password_confirm"
-                name="password_confirm"
+                id="passwordConfirm"
+                name="passwordConfirm"
                 placeholder="Konfirmasi Password"
                 className="w-[40rem] p-2 mt-2 border border-gray-300 rounded"
               />
+              <ErrorMessage name="passwordConfirm" component={'p'} className='text-red-500'/>
             </div>
             <p className='flex justify-end text-blue-700'>Sudah punya akun? <a href="/auth/login">Login di sini</a></p>
             <button
               type="submit"
+              disabled={isSubmitting}
               className="mt-4 w-[40rem] p-2 bg-blue-500 text-white rounded"
             >
               Register
             </button>
-          </form>
+            </Form>  
+            )}
+          </Formik>
         </div>
       </div>
     </div>
